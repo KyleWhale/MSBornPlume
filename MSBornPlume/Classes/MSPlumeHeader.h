43c4d8424d45eccbc12e7949d143a3f551bc13f0
kylej <kylej@bluewhaletech.pro>
2023-06-19 15:14:43 +0800

    header

---
+++ MSBornPlume/Classes/MSPlumeHeader.h
@@ -0,0 +1,27 @@
+
+#if __has_include(<MSBornPlume/MSPlumeHeader.h>)
+#import <MSBornPlume/MSBornPlume.h>
+#import <MSBornPlume/MSPlume.h>
+#import <MSBornPlume/UIView+MSAnimationAdded.h>
+#import <MSBornPlume/MSSubtitlePopupControllerDefines.h>
+#import <MSBornPlume/MSSubtitleItem.h>
+#import <MSBornPlume/MSPlumeResource.h>
+#import <MSBornPlume/MSPlumeResource+MSSubtitlesAdd.h>
+#import <MSBornPlume/MSRotationFullscreenViewController.h>
+#import <MSBornPlume/MSDeviceVolumeAndBrightness.h>
+#import <MSBornPlume/MSControlLayerDefines.h>
+#import <MSBornPlume/MSRotationManager.h>
+#else
+#import "MSBornPlume.h"
+#import "MSPlume.h"
+#import "UIView+MSAnimationAdded.h"
+#import "MSSubtitlePopupControllerDefines.h"
+#import "MSSubtitleItem.h"
+#import "MSPlumeResource.h"
+#import "MSPlumeResource+MSSubtitlesAdd.h"
+#import "MSRotationFullscreenViewController.h"
+#import "MSDeviceVolumeAndBrightness.h"
+#import "MSControlLayerDefines.h"
+#import "MSRotationManager.h"
+
+#endif
